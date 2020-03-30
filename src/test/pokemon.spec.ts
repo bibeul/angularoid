9506f090a69cfb478967757fb975b34d49d45375
nathan bessa <nathan.besssa@gmail.com>
2020-03-30 12:01:52 +0200

    test(battle): add battle test

--- src/test/pokemon.spec.ts
+++ src/test/pokemon.spec.ts
@@ -2,9 +2,15 @@ import {Battle} from "../Battle";
 import {Pokemon} from "../Pokemon";
 
 describe('Test Pokemon.ts', () => {
-    test('should say', () => {
-        const pika: Pokemon = new Pokemon("pikachu", 10);
-        const draco: Pokemon = new Pokemon("dracofeu", 5);
+    const pika: Pokemon = new Pokemon("pikachu", 10);
+    const salamech: Pokemon = new Pokemon("salamech", 11);
+    const draco: Pokemon = new Pokemon("dracofeu", 5);
+
+    test('should pick pikachu', () => {
         expect(Battle.fightFirst(pika, draco)).toBe(pika);
-    })
+    });
+
+    test('should pick salamech', () => {
+        expect(Battle.fightFirst(pika, salamech)).toBe(salamech);
+    });
 });
